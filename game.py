3fc98eeb5677ecb1e6f662462a0a8c8f82b1d51f
larazaki <lkz3@georgetown.edu>
2022-02-02 20:35:44 -0500

    User Choice, Validations and Computer Choice

--- game.py
+++ game.py
@@ -5,3 +5,45 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+
+
+#WELCOME MESSAGE
+
+print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
+
+# ASK FOR USER INPUT
+
+user_choice = input("Please choose one of: 'rock', 'paper', scissors':")
+
+print("You chose:", user_choice)
+
+
+#VALIDATIONS
+
+validChoice = user_choice.lower()
+
+if validChoice != ("rock" or "paper" or "scissors"):
+    print("The choice you entered wasn't valid, sorry!")
+    quit()
+
+#COMPUTER CHOICE
+
+import random
+
+options = ["rock", "paper", "scissors"]
+
+computer_choice = random.choice(options)
+
+print("The computer chose:", computer_choice)
+
+
+#DETERMINE THE WINNER
+
+
+
+
+
+#FINAL RESULTS
+
+
+
